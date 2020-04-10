b1a418bd2e1b19bfae15a926ca8721ec7b49fffb
onofresergio <onofresergio@yahoo.com.br>
2020-04-10 14:04:53 -0300

    prod configs

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env) => {
   return {
     entry: './src/app.js',
     output: {
-      path: path.join(__dirname, 'public'),
+      path: path.join(__dirname, 'public', 'dist'),
       filename: 'bundle.js'
     },
     module: {
@@ -42,7 +42,8 @@ module.exports = (env) => {
     devServer: {
       contentBase: path.join(__dirname, 'public'),
       historyApiFallback: true,
-      open: true
+      open: true,
+      publicPath: '/dist/'
     }
   }
 };
